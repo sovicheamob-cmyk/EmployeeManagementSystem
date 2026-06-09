c6fc4206d3ba2e95a2bae670d87055c4a0eb60cf
sovicheamob-cmyk <sovicheamob@gmail.com>
2026-06-09 14:21:40 +0700

    Update

--- obj/Debug/net10.0/EmployeeManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/EmployeeManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f38eda9466468805c282fef149ffc875077c0e14")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
